602ff8e3ec80fe338a1e13362f4576edd63c92f9
Johnston Stott <johnstonstott@gmail.com>
2020-01-26 01:38:56 -0500

    Revert db change

--- server/plays.js
+++ server/plays.js
@@ -1,7 +1,7 @@
 
 var mongoose = require('mongoose');
 var connectionString = 'mongodb://localhost:27017/tunein';
-mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: false});
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
